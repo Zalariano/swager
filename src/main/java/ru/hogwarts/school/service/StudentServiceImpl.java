cff20d980adeab23ac9c9f191b35ee16a7e06695
Romchig <romulusto@gmail.com>
2023-12-04 00:08:33 +0100

    Не запускается спринг делал все по разбору и уроку не пойму где не так в зависимостях или в чем то еще

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,10 +1,11 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Service
 public class StudentServiceImpl implements StudentService {
 
     private Map<Long, Student> studentMap = new HashMap<>();
